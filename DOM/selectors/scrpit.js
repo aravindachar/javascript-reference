ab1f82b57abd4f573a7602dd1abe8dde5a47125d
Aravinda Achar <141430809+aravindachar@users.noreply.github.com>
2024-11-25 19:07:19 +0530

    Add files via upload

---
+++ DOM/selectors/scrpit.js
@@ -0,0 +1,20 @@
+// types of selectros 
+// get Element by Tag
+// var a = document.getElementsByTagName('li');
+// console.log(a)
+
+// get Element by Class
+// var b = document.getElementsByClassName('name');
+// console.log(b)
+
+// get Element by ID
+// var c = document.getElementById('idselect')
+// console.log(c)
+
+// query Selector
+var d = document.querySelector('.name')
+console.log(d)
+var  a = document.querySelector('h1');
+console.log(a)
+// Query selectAll()
+// console.log('aravinda')
