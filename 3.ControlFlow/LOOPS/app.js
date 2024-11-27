fe8fa6fcf0fa9b46b9034309feb9e9204ddb0739
Aravinda Achar <141430809+aravindachar@users.noreply.github.com>
2024-11-27 21:05:29 +0530

    Add files via upload

---
+++ 3.ControlFlow/LOOPS/app.js
@@ -0,0 +1,52 @@
+//conditional statement
+let a = 32;
+const b = 33;
+if(a < b){
+    console.log('the main');
+}
+else{
+    console.log('the man is guy');
+}
+// SWITCH STATEMENT
+let x = "0";
+
+switch (x) {
+  case 0:
+    text = "Off";
+    break;
+  case 1:
+    text = "On";
+    break;
+  default:
+    text = "No value found";
+}
+//for loop 
+// for (initialExpression; condition; incrementExpression) {}
+
+for (let i = 0; i <= 5; i++) {
+    console.log("Hello World", i);
+  }
+  
+  // EXERCISE
+  // ... (let ; = ; < ; ) {
+  //   console.log(i);
+  // }
+  // while (condition) {
+//   // code block to be executed
+// }
+
+// ----------------
+let i = 1;
+
+while (i <= 5) {
+  console.log("Hello World", i);
+  i++;
+}
+// do wile loop 
+let i = 1;
+
+do {
+  console.log("Hello World", i);
+  i++;
+} while (i <= 5);
+// ----------------
